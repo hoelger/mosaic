be19ba20320a3245efe443f666ec721953df4624
Georg Hoelger <georg.hoelger@fokus.fraunhofer.de>
2024-09-06 18:39:08 +0200

    Add traffic jam recognition

--- app/playground/jam-reroute/src/main/java/org/eclipse/mosaic/app/playground/JamRerouteApp.java
+++ app/playground/jam-reroute/src/main/java/org/eclipse/mosaic/app/playground/JamRerouteApp.java
@@ -18,18 +18,41 @@ package org.eclipse.mosaic.app.playground;
 import org.eclipse.mosaic.fed.application.app.AbstractApplication;
 import org.eclipse.mosaic.fed.application.app.api.VehicleApplication;
 import org.eclipse.mosaic.fed.application.app.api.os.VehicleOperatingSystem;
-import org.eclipse.mosaic.lib.enums.SensorType;
 import org.eclipse.mosaic.lib.objects.vehicle.VehicleData;
 import org.eclipse.mosaic.lib.util.scheduling.Event;
-import org.eclipse.mosaic.rti.TIME;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class JamRerouteApp extends AbstractApplication<VehicleOperatingSystem> implements VehicleApplication {
 
+    List<Double> lastDistances;
+    int LAST_DISTANCES_MAXLEN = 20; // number of seconds to remember
+    double JAM_THRESHOLD = 5; // moving less than JAM_THRESHOLD meter within LAST_DISTANCES_MAXLEN seconds will trigger a traffic jam alarm
+
     @Override
     public void onVehicleUpdated(@Nullable VehicleData previousVehicleData, @Nonnull VehicleData updatedVehicleData) {
+        double previous = 0;
+        if (Objects.nonNull(previousVehicleData)) {
+             previous = previousVehicleData.getDistanceDriven();
+        }
+        double delta = updatedVehicleData.getDistanceDriven() - previous;
+        // getLog().infoSimTime(this, "delta distance: {}",  delta);
+        lastDistances.add(delta);
+        //getLog().infoSimTime(this, "dd: {} sum: {}", updatedVehicleData.getDistanceDriven(), sum);
+        if (lastDistances.size() > LAST_DISTANCES_MAXLEN) {
+            lastDistances.subList(0, lastDistances.size() - LAST_DISTANCES_MAXLEN).clear();
+        }
+        //getLog().infoSimTime(this, "lastDistances {}", lastDistances);
+        double lastDistancesSum = lastDistances.stream().mapToDouble(Double::doubleValue).sum();
+        getLog().infoSimTime(this, "lastDistancesSum {}", lastDistancesSum);
+
+        if (lastDistancesSum < JAM_THRESHOLD) {
+            // TRIGGER ALARM
+        }
     }
 
     @Override
@@ -39,6 +62,7 @@ public class JamRerouteApp extends AbstractApplication<VehicleOperatingSystem> i
     @Override
     public void onStartup() {
         getLog().infoSimTime(this, "Initialize application");
+        this.lastDistances = new ArrayList<Double>();
     }
 
     @Override
